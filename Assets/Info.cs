05d4fe17bbcf46e203c773b1487b10d8cf4507a1
Ben Keenan <ben@ben.ie>
2015-03-06 19:57:56 +0000

    Initial commit
    Commit

---
+++ Assets/Info.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Info : MonoBehaviour {
+
+	public Text infoText;
+
+	// Use this for initialization
+	void Start () {
+
+		string infoString = "Level: " + Application.loadedLevelName +
+			"\nPlatform: " + Application.platform;
+
+		infoText.text = infoString;
+	}
+}
